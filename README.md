c1801e4e0c3a613d72f988d545c9af19465e1137
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2022-12-13 22:43:24 +0100

    make it more reliable

---
+++ README.md
@@ -0,0 +1,9 @@
+Installation
+============
+
+```
+cd $HOME/.local/share/gnome-shell/extensions/
+git clone git@github.com:harentius/gnome-nightscout-bg.git
+or
+https://github.com/harentius/gnome-nightscout-bg.git
+```
--- extension.js
+++ extension.js
@@ -20,171 +20,218 @@ const Soup = imports.gi.Soup;
 const _httpSession = new Soup.SessionAsync();
 Soup.Session.prototype.add_feature.call(_httpSession, new Soup.ProxyResolverDefault());
 
-const roundUsing = function(func, prec, value){
-  var temp = value * Math.pow(10, prec)
-  temp = func(temp);
-  return temp / Math.pow(10, prec)
+class Client {
+  constructor(host) {
+    this.host = host;
+  }
+
+  async getCurrentBg() {
+    let success = false;
+    let retry = 1;
+    let result = { sgv: undefined, direction: undefined };
+
+    while (!success && retry <= 3) {
+      try {
+        const entries = await this._makeHttpRequest('GET', '/api/v1/entries/sgv?count=1');
+
+        if (!Array.isArray(entries) || entries.length < 1) {
+          throw Error('Unsupported response format');
+        }
+
+        const entry = entries[0];
+
+        if (!entry.sgv) {
+          throw Error('Unsupported response format');
+        }
+
+        success = true;
+
+        result = { sgv: entry.sgv, direction: entry.direction };
+      } catch (e) {
+        log(e);
+      } finally {
+        retry++;
+      }
+    }
+
+    return result;
+  }
+
+  async getCurrentIOB() {
+    let success = false;
+    let retry = 1;
+    let result = undefined;
+
+    while (!success && retry <= 3) {
+      try {
+        const entries = await this._makeHttpRequest('GET', '/api/v1/devicestatus?count=1');
+
+        if (!Array.isArray(entries) || entries.length < 1) {
+          throw Error('Unsupported response format');
+        }
+
+        const entry = entries[0];
+
+        if (!entry.openaps) {
+          throw Error('Unsupported response format');
+        }
+
+        const openaps = entry.openaps;
+
+        if (!openaps.IOB && !openaps.iob && (openaps.iob && !openaps.iob.iob)) {
+          throw Error('Unsupported response format');
+        }
+
+        success = true;
+        result = openaps.IOB || openaps.iob.iob;
+      } catch (e) {
+        log(e);
+      } finally {
+        retry++;
+      }
+    }
+
+    return result;
+  }
+
+  async _makeHttpRequest(method, uri) {
+    uri = `${this.host}${uri}`;
+
+    return new Promise((resolve, reject) => {
+      log(`Making a ${method} request to ${uri}`);
+      const request = Soup.Message.new(method, uri);
+      request.request_headers.append('accept', 'application/json');
+      _httpSession.queue_message(request, (_httpSession, message) => {
+        if (message.status_code === 200) {
+          resolve(JSON.parse(message.response_body.data));
+        } else {
+          log(`Failed to acquire request (${message.status_code})`);
+          reject(`Failed to acquire request (${message.status_code})`);
+        }
+      });
+    });
+  }
+}
+
+class DataProvider {
+  constructor(onDataUpdated) {
+    this._client = new Client(host);
+    this._onDataUpdated = onDataUpdated;
+    this._bgInterval = null;
+    this._iobInterval = null;
+    this.iob = undefined;
+    this.bg = {
+      sgv: undefined,
+      direction: undefined,
+    };
+  }
+
+  start() {
+    this._fetchBG();
+    this._fetchIOB();
+
+    this._bgInterval = setInterval(() => {
+      this._fetchBG();
+    }, updatePeriod);
+
+    this._iobInterval = setInterval(() => {
+      this._fetchIOB();
+    }, updatePeriod);
+  }
+
+  stop() {
+    clearInterval(this._bgInterval);
+    clearInterval(this._iobInterval);
+  }
+
+  _fetchBG() {
+    this._client.getCurrentBg().then(v => {
+      this.bg = v;
+      this._triggerUpdate();
+    });
+  }
+
+  _fetchIOB() {
+    this._client.getCurrentIOB().then(v => {
+      this.iob = v;
+      this._triggerUpdate();
+    });
+  }
+
+  _triggerUpdate() {
+    this._onDataUpdated(this.bg.sgv, this.bg.direction, this.iob);
+  }
+}
+
+
+class Presenter {
+  static print(bg, bgDirectionString, iob) {
+    const bgValue = usemmol ? Presenter._roundUsing(Math.ceil, 1, bg * 0.0555).toFixed(1) : bg;
+    const directionGlyph = Presenter._getDirectionGlyph(bgDirectionString);
+    const iobString = iob ? `${iob.toFixed(1)}u` : '';
+
+    return `${bgValue}${directionGlyph}${iobString}`;
+  }
+
+  static _getDirectionGlyph(direction) {
+    const glyphs = {
+      Flat: '→',
+      FortyFiveUp: '↗',
+      FortyFiveDown: '↘',
+      SingleDown: '↓',
+      DoubleDown: '↓↓',
+      TripleDown: '↓↓↓',
+      SingleUp: '↑',
+      DoubleUp: '↑↑',
+      TripleUp: '↑↑↑',
+    }
+
+
+    return glyphs[direction] || '?';
+  }
+
+  static _roundUsing(func, prec, value) {
+    let temp = value * Math.pow(10, prec)
+    temp = func(temp);
+
+    return temp / Math.pow(10, prec)
+  }
 }
 
 class Extension {
-    constructor() {
-        this._indicator = null;
-    }
-    
-    enable() {
-        log(`enabling ${Me.metadata.name}`);
+  constructor() {
+    this._indicator = null;
+    this._dataProvider = null;
+  }
 
-        let indicatorName = `${Me.metadata.name} Indicator`;
-        
-        // Create a panel button
-        this._indicator = new PanelMenu.Button(0.0, indicatorName, false);
-        
-        this.label = new St.Label({
-          text: '',
-        });
-        this._indicator.add_child(this.label);
-        
-        this.updateUI();
-        setInterval(() => {
-          this.updateUI();
-        }, updatePeriod);
+  enable() {
+    log(`enabling ${Me.metadata.name}`);
+    let indicatorName = `${Me.metadata.name} Indicator`;
+    this._indicator = new PanelMenu.Button(0.0, indicatorName, false);
+    this.label = new St.Label({ text: '' });
+    this._indicator.add_child(this.label);
+    Main.panel.addToStatusArea(indicatorName, this._indicator);
 
+    this._dataProvider = new DataProvider((bg, bgDirectionString, iob) => {
+      this.label.set_text(Presenter.print(bg, bgDirectionString, iob));
+    });
+    this._dataProvider.start();
+  }
 
-        // `Main.panel` is the actual panel you see at the top of the screen,
-        // not a class constructor.
-        Main.panel.addToStatusArea(indicatorName, this._indicator);
-    }
+  disable() {
+    log(`disabling ${Me.metadata.name}`);
 
-    updateUI() {
-      try {
-        Promise.all([this.requestCurrentBg(), this.requestDeviceStatus()]).then(values => {
-          if(!this.last) {
-            this.last = values[0];
-          }
+    this._dataProvider.stop();
+    this._dataProvider = null;
 
-          this.makeBGstring(values[0], values[1]);
-          
-          if(values[0] && values[0]._id && (this.last._id !== values[0]._id)) {
-            this.last = values[0];
-          }
-        });
-  
-      } catch (e) {
-        log(e);
-      }
-    }
-
-    makeBGstring(current, status) {
-      let bgString  = "";
-      log(status["openaps"]["iob"]["iob"]);
-      const bgValue = usemmol ? roundUsing(Math.ceil, 1, current.sgv * 0.0555).toFixed(1) : current.sgv;
-      bgString += bgValue;
-      switch(current.direction) {
-        case 'Flat':
-          bgString += ' →';
-          break;
-        case 'FortyFiveUp':
-          bgString += ' ⬈';
-          break;
-        case 'FortyFiveDown':
-          bgString += ' ⬊';
-            break;
-        case 'SingleDown':
-          bgString += ' ↓';
-          break;
-        case 'DoubleDown':
-          bgString += ' ↓↓';
-          break;
-        case 'TripleDown':
-          bgString += ' ↓↓↓';
-          break;
-        case 'SingleUp':
-          bgString += ' ↑';
-          break;
-        case 'DoubleUp':
-          bgString += ' ↑↑';
-          break;
-        case 'TripleUp':
-          bgString += ' ↑↑↑';
-          break;
-        default:
-          break;
-      }
-      
-      if(showiob) {
-        try {
-          const iob = status["openaps"]["iob"]["iob"];
-          bgString += iob.toFixed(1) + 'u';
-          // bgString += "  (IoB: " + status.pump.iob.bolusiob + "U)";
-        } catch (e) {
-          log(e);
-        }
-      }
-      
-      if(showMissing && this.last) {
-        const lastDate = this.last.date;
-        const currentDate = Date.now();
-        const minutesAgo = Math.floor((currentDate - lastDate) / 60 / 1000);
-        if(minutesAgo > showMissingInterval) {
-          bgString = "!Last " + minutesAgo + " m ago!   " + bgString;
-        }
-      }
-      this.label.set_text(bgString);
-
-    }
-    
-    requestCurrentBg() {
-      return this.makeHttpRequest('GET', `${host}/api/v1/entries/current`, (resolve, message) => {
-        let current = {};
-        log('Requested current state' + message.response_body.data);
-        const entries = JSON.parse(message.response_body.data);
-        if(entries.length > 0) {
-          current = entries[0];
-        }
-        resolve(current)
-      });
-    }
-    
-    requestDeviceStatus() {
-      return this.makeHttpRequest('GET', `${host}/api/v1/devicestatus?count=1`, (resolve, message) => {
-        let status = {};
-        log('Requested device status' + message.response_body.data);
-        const statuses = JSON.parse(message.response_body.data);
-        if(statuses.length > 0) {
-          status = statuses[0];
-        }
-        resolve(status);
-      });
-    }
-    
-    makeHttpRequest(method, uri, cb) {
-      uri = uri.replace(/([^:])\/{2,}/, '$1/');
-      return new Promise((resolve, reject) => {
-        log(`Making a ${method} request to ${uri}`);
-        const request = Soup.Message.new(method, uri);
-        request.request_headers.append('accept', 'application/json');
-        _httpSession.queue_message(request, (_httpSession, message) => {
-          if (message.status_code === 200) {
-            cb(resolve, message);
-          } else {
-            log(`Failed to acquire request (${message.status_code})`);
-            reject(`Failed to acquire request (${message.status_code})`);
-          }
-        });
-      });
-    }
-
-    disable() {
-      log(`disabling ${Me.metadata.name}`);
-
-      this._indicator.destroy();
-      this._indicator = null;
+    this._indicator.destroy();
+    this._indicator = null;
   }
 }
 
 
 function init() {
     log(`initializing ${Me.metadata.name}`);
-    
+
     return new Extension();
 }
